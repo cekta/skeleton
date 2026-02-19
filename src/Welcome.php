36c8c5ec3279498c5738a6deaf79915d15ee3d7d
Evgeniy Kuvshinov <smpl@itmywork.com>
2026-02-20 01:01:33 +0500

    Handler via Response\*Factory

--- src/Welcome.php
+++ src/Welcome.php
@@ -4,17 +4,22 @@ declare(strict_types=1);
 
 namespace App;
 
-use Cekta\Framework\HTTP\Response;
+use Cekta\Framework\HTTP\Response\JSONFactory;
 use Cekta\Framework\HTTP\Route;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
 #[Route\GET('/')]
-class Welcome implements RequestHandlerInterface
+final readonly class Welcome implements RequestHandlerInterface
 {
+    public function __construct(
+        private JSONFactory $factory
+    ) {
+    }
+
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        return Response::json(['message' => 'welcome to cekta']);
+        return $this->factory->create(['message' => 'welcome to cekta']);
     }
 }
