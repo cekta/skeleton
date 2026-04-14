5842286daf9a3fe98d3a3d8975490e6cd7d3413a
Evgeniy Kuvshinov <smpl@itmywork.com>
2026-04-15 03:33:28 +0500

    add postgres, cekta/migrator, cekta/roadrunner

--- src/Welcome.php
+++ src/Welcome.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace App;
 
-use Cekta\Framework\HTTP\Response\JSONFactory;
-use Cekta\Framework\HTTP\Route;
+use Cekta\RoadRunner\Response\JSONFactory;
+use Cekta\RoadRunner\Route;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
